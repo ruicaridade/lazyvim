e1f6d5a2743763042b32c1339d7e2496d33f7cca
Rui Caridade <ruidanielcaridade@gmail.com>
2025-10-01 14:29:03 +0100

    feat: install sidekick

--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -11,4 +11,21 @@ return {
       },
     },
   },
+  {
+    "folke/sidekick.nvim",
+    opts = {},
+    keys = {
+      {
+        "<Tab>",
+        function()
+          -- if there is a next edit, jump to it, otherwise apply it if any
+          if not require("sidekick").nes_jump_or_apply() then
+            return "<tab>" -- fallback to normal tab
+          end
+        end,
+        expr = true,
+        desc = "Goto/Apply Next Edit Suggestion",
+      },
+    },
+  },
 }
